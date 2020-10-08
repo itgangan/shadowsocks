ae0a4ba6cecfce26bf723b2f0856d6bff5f55837
甘祥勇 <ganxiangyong@travelsky.com>
2020-10-08 23:10:05 +0800

    加入web导出功能

---
+++ src/main/java/com/itgangan/shadowsocks/ShadowsocksApplication.java
@@ -0,0 +1,13 @@
+package com.itgangan.shadowsocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShadowsocksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShadowsocksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itgangan/shadowsocks/ShadowsocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itgangan.shadowsocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShadowsocksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
